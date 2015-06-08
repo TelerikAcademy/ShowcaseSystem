9b5469ebc015277a650e308a1574e74c6f59f7d8
Nikolay Kostov <admin@nikolay.it>
2015-06-08 12:10:42 +0300

    Created Showcase.Server.ViewModels project to contain all view models for the server API project

---
+++ Source/Server/Showcase.Server.ViewModels/HomePage/HomePageProjectViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Showcase.Server.ViewModels.HomePage
+{
+    public class HomePageProjectViewModel
+    {
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+    }
+}
