62ba6a496c4fc1c9fcb3418715aa8e64f26227c4
Nikolay Kostov <admin@nikolay.it>
2015-06-08 15:44:42 +0300

    Added sample HomePageController with hardcoded values for Get action

---
+++ Source/Server/Showcase.Server.Api/Controllers/HomePageController.cs
@@ -0,0 +1,32 @@
+﻿namespace Showcase.Server.Api.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+
+    using Showcase.Server.ViewModels.HomePage;
+
+    public class HomePageController : ApiController
+    {
+        public IEnumerable<HomePageProjectViewModel> Get()
+        {
+            return new List<HomePageProjectViewModel>
+                       {
+                           new HomePageProjectViewModel
+                               {
+                                   Name = "Telerik Academy Learning System",
+                                   Author = "Telerik Academy"
+                               },
+                           new HomePageProjectViewModel
+                               {
+                                   Name = "Telerik Academy Test System",
+                                   Author = "Telerik Academy"
+                               },
+                           new HomePageProjectViewModel
+                               {
+                                   Name = "Telerik Academy Showcase System",
+                                   Author = "Telerik Academy"
+                               },
+                       };
+        }
+    }
+}
