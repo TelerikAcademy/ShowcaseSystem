a10cf96421286984ab844493fc81e08b7e99ac0d
Nikolay Kostov <admin@nikolay.it>
2015-06-08 16:10:16 +0300

    Fixed casing differences between Web API serializer and Angular best practices (camel case)

--- Source/Server/Showcase.Server.Api/App_Start/WebApiConfig.cs
+++ Source/Server/Showcase.Server.Api/App_Start/WebApiConfig.cs
@@ -3,14 +3,16 @@
     using System.Web.Http;
     using System.Web.Http.Cors;
 
+    using Newtonsoft.Json.Serialization;
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
             config.MapHttpAttributeRoutes();
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
 
             var cors = new EnableCorsAttribute("*", "*", "*");
             config.EnableCors(cors);
