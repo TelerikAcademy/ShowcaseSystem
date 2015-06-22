ea6c800d0f93b1e750171c292e1038f19be68859
Zdravko Georgiev <skinsomnia@gmail.com>
2015-06-22 18:30:32 +0300

    Initial Models added

---
+++ Source/Showcase.Data.Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace Showcase.Data.Models
+{
+    using Showcase.Data.Common.Models;
+    
+    public class Comment : AuditInfo
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ Source/Showcase.Data.Models/Like.cs
@@ -0,0 +1,11 @@
+﻿namespace Showcase.Data.Models
+{
+    using Showcase.Data.Common.Models;
+    
+    public class Like : AuditInfo
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
