fb788ead59c234e30129fdd34fdcf9a3d680dc7d
Zdravko Georgiev <skinsomnia@gmail.com>
2015-06-22 18:31:49 +0300

    Common models and repositories added

---
+++ Source/Showcase.Data.Common/Repositories/IRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace Showcase.Data.Common.Repositories
+{
+    using System;
+    using System.Linq;
+
+    public interface IRepository<T> : IDisposable where T : class
+    {
+        IQueryable<T> All();
+
+        T GetById(object id);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        void Delete(object id);
+
+        void Detach(T entity);
+
+        int SaveChanges();
+    }
+}
