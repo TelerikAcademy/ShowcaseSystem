bdd10ba146ef370ac96ea6bd607f52e13b68a1a3
Zdravko Georgiev <skinsomnia@gmail.com>
2015-06-22 18:34:08 +0300

    Enabled migrations and added connection string

--- Source/Server/Showcase.Server.Api/Global.asax.cs
+++ Source/Server/Showcase.Server.Api/Global.asax.cs
@@ -1,13 +1,20 @@
 ﻿namespace Showcase.Server.Api
 {
+    using System.Data.Entity;
+    using System.Web;
     using System.Web.Http;
     using System.Web.Mvc;
     using System.Web.Routing;
 
-    public class WebApiApplication : System.Web.HttpApplication
+    using Showcase.Data;
+    using Showcase.Data.Migrations;
+
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ShowcaseDbContext, Configuration>());
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
