7f4d85ac4420e3fa6e12756ec49e318a295a0405
Zdravko Georgiev <skinsomnia@gmail.com>
2015-06-24 16:55:41 +0300

    Added Image data model

---
+++ Source/Showcase.Data.Models/Image.cs
@@ -0,0 +1,13 @@
+﻿namespace Showcase.Data.Models
+{
+    public class Image
+    {
+        public string Id { get; set; }
+
+        public string OriginalFilename { get; set; }
+
+        public string Url { get; set; }
+
+        public string FileExtension { get; set; }
+    }
+}
