ee11d9cf377697cb10eca7d0b2cc2329b02c09e5
Zdravko Georgiev <skinsomnia@gmail.com>
2015-07-15 17:51:02 +0300

    Added UserId to Like model and Likes collection to User model

--- Source/Data/Showcase.Data.Models/Like.cs
+++ Source/Data/Showcase.Data.Models/Like.cs
@@ -7,5 +7,11 @@
         public int Id { get; set; }
 
         public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public virtual Project Project { get; set; }
     }
 }
--- Source/Data/Showcase.Data.Models/User.cs
+++ Source/Data/Showcase.Data.Models/User.cs
@@ -14,5 +14,7 @@
         public string Username { get; set; }
 
         public ICollection<Project> Projects { get; set; }
+
+        public ICollection<Like> Likes { get; set; }
     }
 }
