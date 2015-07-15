b76658b2aa7adad88f72deca0eff85e14ac24daa
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-15 16:52:50 +0300

    Added statistics directive

---
+++ Source/Client/Showcase.Client.App/app/home-page/statistics-directive.js
@@ -0,0 +1,25 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('showcaseSystem.directives')
+        .directive('statistics', ['jQuery', statisticsDirective]);
+
+    function statisticsDirective(jQuery) {
+        return {
+            restrict: 'A',
+            templateUrl: '/app/home-page/statistics-directive.html',
+            scope: {
+                stats: '='
+            },
+            link: function(scope, element) {
+                element.find('[data-to]').each(function() {
+                    var $counter = jQuery(this);
+                    $counter.appear(function() {
+                        $counter.countTo();
+                    }, {accX:0, accY: -50});
+                })
+            }
+        };
+    }
+}());
