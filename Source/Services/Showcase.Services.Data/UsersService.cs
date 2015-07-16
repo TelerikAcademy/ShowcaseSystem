6ef13cc7e19b1d8325064f3d1eb5c01766e34ba1
Zdravko Georgiev <skinsomnia@gmail.com>
2015-07-16 14:01:21 +0300

    Added Users Service

---
+++ Source/Services/Showcase.Services.Data/UsersService.cs
@@ -0,0 +1,28 @@
+﻿namespace Showcase.Services.Data
+{
+    using System;
+    using System.Linq;
+
+    using Showcase.Data.Common.Repositories;
+    using Showcase.Data.Models;
+    using Showcase.Services.Data.Contracts;
+
+    public class UsersService : IUsersService
+    {
+        private readonly IRepository<User> users;
+
+        public UsersService(IRepository<User> users)
+        {
+            this.users = users;
+        }
+
+        public int GetUserId(string username)
+        {
+            return this.users
+                .All()
+                .Where(u => u.Username == username)
+                .Select(u => u.Id)
+                .FirstOrDefault();
+        }
+    }
+}
