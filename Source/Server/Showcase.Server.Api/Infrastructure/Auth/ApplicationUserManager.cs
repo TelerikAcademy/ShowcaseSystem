1d3794b2bd2dbd0726bc9fede9460e7fa4158deb
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-16 17:02:34 +0300

    Added OAuth pipeline to the solution (it is still not configured)

---
+++ Source/Server/Showcase.Server.Api/Infrastructure/Auth/ApplicationUserManager.cs
@@ -0,0 +1,49 @@
+﻿namespace Showcase.Server.Api.Infrastructure.Auth
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity.Owin;
+    using Microsoft.Owin;
+
+    using Showcase.Data;
+    using Showcase.Data.Models;
+
+    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
+    public class ApplicationUserManager : UserManager<User>
+    {
+        public ApplicationUserManager(IUserStore<User> store)
+            : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
+        {
+            var manager = new ApplicationUserManager(new UserStore<User>(context.Get<ShowcaseDbContext>()));
+
+            // Configure validation logic for usernames
+            manager.UserValidator = new UserValidator<User>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+
+            // Configure validation logic for passwords
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 3,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
+            };
+
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider = new DataProtectorTokenProvider<User>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+
+            return manager;
+        }
+    }
+}
