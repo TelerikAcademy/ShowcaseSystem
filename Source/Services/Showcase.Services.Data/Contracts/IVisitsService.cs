d0b2cccea8eb7597c7b0b2ed32cc6dc84b36fa2e
Zdravko Georgiev <skinsomnia@gmail.com>
2015-07-16 14:01:58 +0300

    Added Visits Service

---
+++ Source/Services/Showcase.Services.Data/Contracts/IVisitsService.cs
@@ -0,0 +1,9 @@
+﻿namespace Showcase.Services.Data.Contracts
+{
+    using Showcase.Services.Common;
+
+    public interface IVisitsService : IService
+    {
+        void VisitProject(int projectId, string username);
+    }
+}
