fc902dccbeedaf552b313811b4f7f0b9b4d71775
Zdravko Georgiev <skinsomnia@gmail.com>
2015-07-16 11:04:45 +0300

    Added comments to Project details page

--- Source/Client/Showcase.Client.App/app/project-details/project-details-controller.js
+++ Source/Client/Showcase.Client.App/app/project-details/project-details-controller.js
@@ -1,17 +1,18 @@
 ﻿(function () {
     'use strict';
 
-    angular
-        .module('showcaseSystem.controllers')
-        .controller('ProjectDetailsController', ['projectDetailsData', '$routeParams', projectDetailsController]);
-
-    function projectDetailsController(projectDetailsData, $routeParams) {
+    var projectDetailsController = function projectDetailsController(projectDetailsData, $routeParams) {
         var vm = this;
         var id = $routeParams['id'];
 
         projectDetailsData.getProject(id)
             .then(function (project) {
+                console.log(project);
                 vm.project = project;
             });
     }
+
+    angular
+        .module('showcaseSystem.controllers')
+        .controller('ProjectDetailsController', ['projectDetailsData', '$routeParams', projectDetailsController]);    
 }());
