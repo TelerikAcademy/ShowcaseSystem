799a6ccd70c033beee10c7dda63ed1f22ffe12c5
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-16 17:02:34 +0300

    Added OAuth pipeline to the solution (it is still not configured)

---
+++ Source/Server/Showcase.Server.Api/Infrastructure/Auth/ApplicationOAuthBearerAuthenticationProvider.cs
@@ -0,0 +1,33 @@
+﻿namespace Showcase.Server.Api.Infrastructure.Auth
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.Owin.Security.OAuth;
+
+    public class ApplicationOAuthBearerAuthenticationProvider : OAuthBearerAuthenticationProvider
+    {
+        public override Task RequestToken(OAuthRequestTokenContext context)
+        {
+            if (string.IsNullOrEmpty(context.Token))
+            {
+                // try to find bearer token in a cookie 
+                var tokenCookie = context.OwinContext.Request.Cookies["authentication"];
+                if (!string.IsNullOrEmpty(tokenCookie))
+                {
+                    context.Token = tokenCookie;
+                }
+                else
+                {
+                    // try to find bearer token in the standard Authorization header - should not occur for the web app.
+                    var tokenHeader = context.OwinContext.Request.Headers["Authorization"];
+                    if (!string.IsNullOrEmpty(tokenHeader))
+                    {
+                        context.Token = tokenHeader;
+                    }
+                }
+            }
+
+            return Task.FromResult<object>(null);
+        }
+    }
+}
