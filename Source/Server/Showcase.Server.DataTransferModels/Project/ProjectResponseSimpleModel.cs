198cdb595247e707fb0e6371b426fd924a1ec333
EvlogiHr <evlogihristov@yahoo.com>
2015-07-17 16:41:32 +0300

    Added simple response model for Projects

---
+++ Source/Server/Showcase.Server.DataTransferModels/Project/ProjectResponseSimpleModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Showcase.Server.DataTransferModels.Project
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using AutoMapper;
+    using MissingFeatures;
+    using Newtonsoft.Json;
+
+    using Showcase.Data.Models;
+    using Showcase.Server.Common.Mapping;
+
+    public class ProjectResponseSimpleModel : IMapFrom<Project>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string MainImageUrl { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<Project, ProjectResponseSimpleModel>()
+                .ForMember(pr => pr.Name, opt => opt.MapFrom(pr => pr.Title))
+                .ForMember(pr => pr.MainImageUrl, opt => opt.MapFrom(pr => pr.MainImage.Url + "." + pr.MainImage.FileExtension));
+        }
+    }
+}
