4d4f75542064157718ceea026cd08ea6a24075e1
Zdravko Georgiev <skinsomnia@gmail.com>
2015-07-17 16:42:36 +0300

    Added comments service

---
+++ Source/Server/Showcase.Server.Api/Controllers/CommentsController.cs
@@ -0,0 +1,56 @@
+﻿namespace Showcase.Server.Api.Controllers
+{
+    using System.Linq;
+    using System.Web.Http;
+
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+
+    using Showcase.Data.Models;
+    using Showcase.Server.Api.Infrastructure.Extensions;
+    using Showcase.Server.DataTransferModels.Project;
+    using Showcase.Services.Data.Contracts;
+
+    public class CommentsController : ApiController
+    {
+        private const int PageSize = 5;
+
+        private readonly ICommentsService comments;
+
+        public CommentsController(ICommentsService comments)
+        {
+            this.comments = comments;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(int id, CommentRequestModel comment)
+        {
+            if (comment == null || !this.ModelState.IsValid)
+            {
+                return this.Data(false, "Invalid data.");
+            }
+
+            var username = this.User.Identity.Name;
+
+            var postedComment = this.comments.PostComment(id, comment.CommentText, username);
+
+            var model = Mapper.Map<Comment, CommentResponseModel>(postedComment);
+
+            return this.Data(model);
+        }
+
+        [Route("api/Comments/{id}/{page}")]
+        public IHttpActionResult Get(int id, int page)
+        {
+            var model = this.comments
+                .GetAllComments(id)
+                .Skip(page * PageSize)
+                .Take(PageSize)
+                .Project()
+                .To<CommentResponseModel>()
+                .ToList();
+
+            return this.Data(model);
+        }
+    }
+}
---
+++ Source/Services/Showcase.Services.Data/Contracts/ICommentsService.cs
@@ -0,0 +1,14 @@
+﻿namespace Showcase.Services.Data.Contracts
+{
+    using System.Linq;
+
+    using Showcase.Data.Models;
+    using Showcase.Services.Common;
+
+    public interface ICommentsService : IService
+    {
+        Comment PostComment(int id, string commentText, string username);
+
+        IQueryable<Comment> GetAllComments(int id);
+    }
+}
