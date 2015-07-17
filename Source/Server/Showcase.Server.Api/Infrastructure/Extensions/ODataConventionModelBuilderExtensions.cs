3c5f6a7cffba9cd47063cf9e0125b8bf224d02d9
EvlogiHr <evlogihristov@yahoo.com>
2015-07-17 16:39:23 +0300

    Added OData Extensions

---
+++ Source/Server/Showcase.Server.Api/Infrastructure/Extensions/ODataConventionModelBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Showcase.Server.Api.Infrastructure.Extensions
+{
+    using System.Web.Http;
+    using System.Web.OData.Builder;
+
+    public static class ODataConventionModelBuilderExtensions
+    {
+        private const string Controller = "Controller";
+
+        public static void EntitySet<TResponse, TController>(this ODataConventionModelBuilder modelBuilder)
+            where TResponse : class
+            where TController : ApiController
+        {
+            var controllerName = typeof(TController).Name.Replace(Controller, string.Empty);
+            modelBuilder.EntitySet<TResponse>(controllerName);
+        }
+    }
+}
---
+++ Source/Server/Showcase.Server.Api/Infrastructure/ODataConfig.cs
@@ -0,0 +1,30 @@
+﻿namespace Showcase.Server.Api.Infrastructure
+{
+    using System.Web.Http;
+    using System.Web.OData.Builder;
+    using System.Web.OData.Extensions;
+
+    using Microsoft.OData.Edm;
+
+    using Showcase.Server.Api.Controllers;
+    using Showcase.Server.Api.Infrastructure.Extensions;
+    using Showcase.Server.DataTransferModels.Project;
+
+    public static class ODataConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapODataServiceRoute(
+                routeName: "OData",
+                routePrefix: "odata",
+                model: GetEdmModel());
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            var modelBuilder = new ODataConventionModelBuilder();
+            modelBuilder.EntitySet<ProjectResponseSimpleModel, ProjectsController>();
+            return modelBuilder.GetEdmModel();
+        }
+    }
+}
