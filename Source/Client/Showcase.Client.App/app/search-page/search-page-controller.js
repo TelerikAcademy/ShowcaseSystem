ee492a8f28827a37d6d6f277c233db14b55f35ea
EvlogiHr <evlogihristov@yahoo.com>
2015-07-20 19:15:28 +0300

    Added search-page-controller, search-page-data and search-page-view Implemented listing with OData Implemented filtering, sorting, paging

---
+++ Source/Client/Showcase.Client.App/app/search-page/search-page-controller.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    'use strict';
+
+    var searchPageController = function searchPageController(searchPageData, $routeParams, $location) {
+
+        var CONSTS = {
+            DESC: 'desc'
+        };
+
+        var vm = this, orderBy, desc;
+        
+        vm.filterOptions = searchPageData.getFilterOptions();
+        console.log(vm.filterOptions);
+        
+        vm.search = function () {
+            $location.search('$orderby', vm.filterOptions.orderby.value + (vm.filterOptions.desc ? ' ' + CONSTS.DESC : ''));
+            $location.search('$top', vm.filterOptions.pageSize);
+            if (!!vm.filterOptions.page) {
+                $location.search('$skip', vm.filterOptions.page);
+            }
+        };
+
+        vm.search();
+        searchPageData.searchProjects($location.url())
+            .then(function (projects) {
+                vm.projects = projects;
+            });
+    };
+
+    angular
+        .module('showcaseSystem.controllers')
+        .controller('searchPageController', ['searchPageData', '$routeParams', '$location', searchPageController]);
+}());
---
+++ Source/Client/Showcase.Client.App/app/search-page/search-page-data.js
@@ -0,0 +1,55 @@
+﻿(function () {
+    'use strict';
+
+    var searchPageData = function searchPageData(data, $routeParams) {
+        var CONSTS = {
+            DESC: 'desc'
+        };
+
+        function searchProjects(oData) {
+            oData = oData || '/search';
+            return data.get('odata' + oData);
+        }
+
+        function getFilterOptions() {
+            var filterOptions = {
+                options: [
+                    { value: 'CreatedOn', name: 'Date' },
+                    { value: 'Visits', name: 'Views' },
+                    { value: 'Likes', name: 'Likes' },
+                    { value: 'Comments', name: 'Comments' },
+                    { value: 'Name', name: 'Name' }
+                ],
+                pageSizes: [4, 8, 16, 32, 64],
+                page: $routeParams.$page || 0,
+            };
+
+            filterOptions.pageSize = $routeParams.$top || filterOptions.pageSizes[1];
+            filterOptions.desc = (function getDesc() {
+                var desc = $routeParams.$orderby && $routeParams.$orderby.indexOf(CONSTS.DESC) > 0;
+                return desc || false;
+            })();
+            filterOptions.orderby = (function getOrderBy() {
+                var orderBy = (filterOptions.options.filter(function (item) {
+                    if (filterOptions.desc) {
+                        return item.value + ' ' + CONSTS.DESC === $routeParams.$orderby;
+                    } else {
+                        return item.value === $routeParams.$orderby;
+                    }
+                })[0]);
+                return orderBy || filterOptions.options[0];
+            })();
+
+            return filterOptions;
+        }
+
+        return {
+            searchProjects: searchProjects,
+            getFilterOptions: getFilterOptions
+        };
+    };
+
+    angular
+        .module('showcaseSystem.data')
+        .factory('searchPageData', ['data', '$routeParams', searchPageData]);
+}());
