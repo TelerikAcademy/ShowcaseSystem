253f752cdba8f95314c544a84b733401d1c10b74
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-20 18:32:40 +0300

    Added angular cookies Renamed common services Added auth and identity services

---
+++ Source/Client/Showcase.Client.App/app/common/auth-service.js
@@ -0,0 +1,45 @@
+﻿(function () {
+    'use strict';
+
+    var auth = function auth($http, $q, $cookies, identity) {
+        var login = function login(user) {
+            var deferred = $q.defer();
+
+            var data = "grant_type=password&username=" + user.username + '&password=' + user.password;
+
+            $http.post('/api/account/login', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+                .success(function (response) {
+                    var tokenValue = response['access_token'];
+
+                    var theBigDay = new Date();
+                    theBigDay.setHours(theBigDay.getHours() + 72);
+
+                    $cookies.put(TOKEN_KEY, tokenValue, { expires: theBigDay });
+
+                    $http.defaults.headers.common.Authorization = 'Bearer ' + tokenValue;
+
+                    deferred.resolve(response);
+                })
+                .error(function (err) {
+                    deferred.reject(err);
+                });
+
+            return deferred.promise;
+        };
+
+        return {
+            login: login,
+            isAuthenticated: function () {
+                return !!$cookies.get(TOKEN_KEY);
+            },
+            logout: function () {
+                $cookies.remove(TOKEN_KEY);
+                $http.defaults.headers.common.Authorization = null;
+            },
+        }
+    };
+
+    angular
+        .module('showcaseSystem.services')
+        .factory('auth', ['$http', '$q', '$cookies', 'identity', auth]);
+}());
---
+++ Source/Client/Showcase.Client.App/app/common/identity-service.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    var identity = function identity() {
+        var currentUser = {};
+
+        return {
+            getUser: function () {
+                return currentUser;
+            },
+            isAuthenticated: function () {
+                return Object.getOwnPropertyNames(currentUser).length !== 0;
+            },
+            setUser: function (user) {
+                currentUser = user;
+            },
+            removeUser: function () {
+                currentUser = {};
+            }
+        };
+    };
+
+    angular
+        .module('showcaseSystem.services')
+        .factory('identity', [identity]);
+}());
