315db628c1e282381a8ef0973dfc4a74c9bd7cc6
EvlogiHr <evlogihristov@yahoo.com>
2015-07-20 19:14:04 +0300

    Added directive for search filters Implemented order-by, page size and ascending/descending

---
+++ Source/Client/Showcase.Client.App/app/search-page/filters-directive.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    var filters = function filters() {
+        return {
+            restrict: 'A',
+            templateUrl: '/app/search-page/filters-directive.html',
+            scope: {
+                filteroptions: '=',
+                search: '&'
+            }
+        };
+    };
+
+    angular.module('showcaseSystem.directives')
+        .directive('filters', [filters]);
+}());
