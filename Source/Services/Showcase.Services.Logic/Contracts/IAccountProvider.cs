2f160ddc339f551d43f876ed47089308357b27b1
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-20 13:29:31 +0300

    Authentication provider added for getting user from telerikacademy.com

---
+++ Source/Services/Showcase.Services.Logic/Contracts/IAccountProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Showcase.Services.Logic.Contracts
+{
+    using Showcase.Data.Models;
+
+    public interface IAccountProvider
+    {
+        User GetAccount(string username, string password);
+    }
+}
