511ddda653fa01c95a6879608a5809ab2cfdde6e
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-20 21:43:42 +0300

    Main menu made clickable

--- Source/Client/Showcase.Client.App/app/common/main-menu-directive.js
+++ Source/Client/Showcase.Client.App/app/common/main-menu-directive.js
@@ -6,7 +6,11 @@
             restrict: 'A',
             templateUrl: '/app/common/main-menu-directive.html',
             link: function (scope, element) {
-                
+                element.on('click', 'li', function () {
+                    var $this = $(this);
+                    $this.parent().children().removeClass('active');
+                    $this.addClass('active');
+                });
             }
         };
     };
