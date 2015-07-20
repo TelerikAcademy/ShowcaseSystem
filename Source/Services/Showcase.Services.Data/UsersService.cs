1830b589104256fc0dd54909eb4ff38c2d01d700
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-20 17:27:14 +0300

    Merge issues fixed

--- Source/Services/Showcase.Services.Data/UsersService.cs
+++ Source/Services/Showcase.Services.Data/UsersService.cs
@@ -72,12 +72,5 @@
                 .All()
                 .FirstOrDefaultAsync(u => u.UserName == username);
         }
-
-        public IQueryable<User> GetByUsername(string username)
-        {
-            return this.users
-                .All()
-                .Where(u => u.Username == username);
-        }
     }
 }
