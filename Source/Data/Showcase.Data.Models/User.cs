81945ed3f5813b74c7d457fccd3c0b0c604712f1
Zdravko Georgiev <skinsomnia@gmail.com>
2015-07-20 15:30:11 +0300

    Updated User db model with AvatarUrl and collections of Likes and Comments

--- Source/Data/Showcase.Data.Models/User.cs
+++ Source/Data/Showcase.Data.Models/User.cs
@@ -7,14 +7,20 @@
         public User()
         {
             this.Projects = new HashSet<Project>();
+            this.Comments = new HashSet<Comment>();
+            this.Likes = new HashSet<Like>();
         }
 
         public int Id { get; set; }
 
         public string Username { get; set; }
 
+        public string AvatarUrl { get; set; }
+
         public ICollection<Project> Projects { get; set; }
 
         public ICollection<Like> Likes { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
     }
 }
