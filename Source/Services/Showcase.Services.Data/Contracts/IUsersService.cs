610c252c8ccd99afd2be77eca078c6d453ff2d47
Zdravko Georgiev <skinsomnia@gmail.com>
2015-07-20 15:32:01 +0300

    Added Users service

--- Source/Services/Showcase.Services.Data/Contracts/IUsersService.cs
+++ Source/Services/Showcase.Services.Data/Contracts/IUsersService.cs
@@ -1,9 +1,14 @@
 ﻿namespace Showcase.Services.Data.Contracts
 {
+    using System.Linq;
+
+    using Showcase.Data.Models;
     using Showcase.Services.Common;
 
     public interface IUsersService : IService
     {
         int GetUserId(string username);
+
+        IQueryable<User> GetByUsername(string username);
     }
 }
