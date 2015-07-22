296182978176e6606728636abf288b5c8b29d784
Zdravko Georgiev <skinsomnia@gmail.com>
2015-07-22 15:42:47 +0300

    Added TeammateResponseModel

---
+++ Source/Server/Showcase.Server.DataTransferModels/User/TeammateResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Showcase.Server.DataTransferModels.User
+{
+    using Showcase.Data.Models;
+    using Showcase.Server.Common.Mapping;
+
+    public class TeammateResponseModel : IMapFrom<User>
+    {
+        public string Username { get; set; }
+
+        public string AvatarUrl { get; set; }
+    }
+}
