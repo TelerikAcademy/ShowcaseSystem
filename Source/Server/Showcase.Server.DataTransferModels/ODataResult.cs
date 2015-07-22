a9db9ed73f492091d652983e875f84c0f4acd6af
EvlogiHr <evlogihristov@yahoo.com>
2015-07-22 17:14:33 +0300

    Added ODataResult to enable count for paging

---
+++ Source/Server/Showcase.Server.DataTransferModels/ODataResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Showcase.Server.DataTransferModels
+{
+    using System.Collections.Generic;
+
+    public class ODataResult<T>
+    {
+        public ODataResult(IEnumerable<T> results, long? count = null)
+        {
+            this.Results = results;
+            this.Count = count;
+        }
+
+        public IEnumerable<T> Results { get; set; }
+
+        public long? Count { get; set; }
+    }
+}
