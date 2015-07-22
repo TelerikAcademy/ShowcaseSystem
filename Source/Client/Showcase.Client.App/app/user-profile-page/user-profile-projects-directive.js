6a97d12e5da920f5cd55fe9fa892730d96ccdaa0
Zdravko Georgiev <skinsomnia@gmail.com>
2015-07-22 12:34:31 +0300

    Moved projects sorting functions to profile projects directive

--- Source/Client/Showcase.Client.App/app/user-profile-page/user-profile-projects-directive.js
+++ Source/Client/Showcase.Client.App/app/user-profile-page/user-profile-projects-directive.js
@@ -7,6 +7,62 @@
             templateUrl: '/app/user-profile-page/user-profile-projects-directive.html',
             scope: {
                 projects: '='
+            },
+            link: function (scope, element) {
+                var arrowDownCss = 'fa fa-long-arrow-down',
+                    arrowUpCss = 'fa fa-long-arrow-up';
+
+                scope.orderBy = '-createdOn';
+
+                scope.sortByDate = function (element, $event) {
+                    var $target = $($event.currentTarget);
+
+                    if (scope.orderBy == '-createdOn') {
+                        scope.orderBy = 'createdOn';
+                        removeArrowClass($target);
+                        $target.find('i').addClass(arrowUpCss);
+                    } else {
+                        scope.orderBy = '-createdOn';
+                        removeArrowClass($target);
+                        $target.find('i').addClass(arrowDownCss);
+                    }
+                };
+
+                scope.sortByVisits = function (element, $event) {
+                    var $target = $($event.currentTarget);
+
+                    if (scope.orderBy == '-visits') {
+                        scope.orderBy = 'visits';
+                        removeArrowClass($target);
+                        $target.find('i').addClass(arrowUpCss);
+                    } else {
+                        scope.orderBy = '-visits';
+                        removeArrowClass($target);
+                        $target.find('i').addClass(arrowDownCss);
+                    }
+                };
+
+                scope.sortByComments = function (element, $event) {
+                    var $target = $($event.currentTarget);
+
+                    if (scope.orderBy == '-comments') {
+                        scope.orderBy = 'comments';
+                        removeArrowClass($target);
+                        $target.find('i').addClass(arrowUpCss);
+                    } else {
+                        scope.orderBy = '-comments';
+                        removeArrowClass($target);
+                        $target.find('i').addClass(arrowDownCss);
+                    }
+                };
+
+                var removeArrowClass = function (element) {
+                    element
+                        .parent()
+                        .find('i')
+                        .removeClass(arrowUpCss)
+                        .removeClass(arrowDownCss);
+                };
             }
         };
     };
