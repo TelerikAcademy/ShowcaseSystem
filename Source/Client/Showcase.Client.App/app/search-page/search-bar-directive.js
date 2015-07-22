476d629bbf0da168f6a3331b7db2d829d0ecea9c
EvlogiHr <evlogihristov@yahoo.com>
2015-07-22 17:15:54 +0300

    Added search bar directive

---
+++ Source/Client/Showcase.Client.App/app/search-page/search-bar-directive.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+    
+    var searchBar = function searchBar() {
+        return {
+            restrict: 'A',
+            templateUrl: '/app/search-page/search-bar-directive.html',
+            scope: {
+                searchTerms: '=',
+            }
+        };
+    };
+
+    angular
+        .module('showcaseSystem.directives')
+        .directive('searchBar', [searchBar]);
+
+}());
