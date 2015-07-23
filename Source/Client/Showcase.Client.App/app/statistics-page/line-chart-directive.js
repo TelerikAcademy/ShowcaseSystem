c3a07a8dd7971a7f0ac6cbc134d18f9bb9ef99b0
Zdravko Georgiev <skinsomnia@gmail.com>
2015-07-23 16:47:11 +0300

    Added line-chart directive

---
+++ Source/Client/Showcase.Client.App/app/statistics-page/line-chart-directive.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    'use strict';
+
+    var lineChartDirective = function lineChartDirective(jQuery) {
+        return {
+            restrict: 'A',
+            templateUrl: '/app/statistics-page/line-chart-directive.html',
+            scope: {
+                stats: '='
+            },
+            link: function (scope, element) {
+                scope.$watch('stats', function (stats) {
+                    if (stats) {
+                        console.log(stats);
+                        var data = {
+                            labels: stats.labels,
+                            datasets: [
+                                {
+                                    label: "My First dataset",
+                                    fillColor: "rgba(220,220,220,0.2)",
+                                    strokeColor: "rgba(220,220,220,1)",
+                                    pointColor: "rgba(220,220,220,1)",
+                                    pointStrokeColor: "#fff",
+                                    pointHighlightFill: "#fff",
+                                    pointHighlightStroke: "rgba(220,220,220,1)",
+                                    data: stats.values
+                                }
+                            ]
+                        };
+
+                        var ctx = $("#line-chart").get(0).getContext("2d");
+                        var chart = new Chart(ctx).Line(data);
+                    }
+                });
+            }
+        };
+    };
+
+    angular
+        .module('showcaseSystem.directives')
+        .directive('lineChart', ['jQuery', lineChartDirective]);
+}());
