5ff09af2b6ef748218c1fd56b9b9f12bb64b7f7c
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-23 17:33:20 +0300

    Added needed methods in RemoteDataService.cs

--- Source/Services/Showcase.Services.Data/Contracts/IRemoteDataService.cs
+++ Source/Services/Showcase.Services.Data/Contracts/IRemoteDataService.cs
@@ -1,10 +1,17 @@
 ﻿namespace Showcase.Services.Data.Contracts
 {
+    using System.Collections.Generic;
+
     using Showcase.Data.Models;
     using Showcase.Services.Common;
+    using Showcase.Services.Data.Models;
 
     public interface IRemoteDataService : IService
     {
         User RemoteLogin(string username, string password);
+
+        IEnumerable<string> SearchByUsername(string username);
+
+        RemoteUserProfile ProfileInfo(string username);
     }
 }
--- Source/Services/Showcase.Services.Data/RemoteDataService.cs
+++ Source/Services/Showcase.Services.Data/RemoteDataService.cs
@@ -1,7 +1,10 @@
 ﻿namespace Showcase.Services.Data
 {
+    using System.Collections.Generic;
+
     using Showcase.Data.Models;
     using Showcase.Services.Data.Contracts;
+using Showcase.Services.Data.Models;
 
     public class RemoteDataService : IRemoteDataService
     {
@@ -11,8 +14,39 @@
             return new User
             {
                 UserName = username,
-                AvatarUrl = "some url",
+                AvatarUrl = "some url", // return small avatar URL here 
                 IsAdmin = true
+            };
+        }
+
+        public IEnumerable<string> SearchByUsername(string username)
+        {
+            // TODO: get from telerikacademy.com all usernames which contain the search
+            return new List<string>
+            {
+                "ivaylo.kenov",
+                "ivaylo.manekenov",
+                "zdravko.georgiev",
+                "zdravko.jelqzkov",
+                "evlogi.hristov",
+                "nikolay.kostov",
+                "kolio",
+                "kolio.TLa"
+            };
+        }
+
+        public RemoteUserProfile ProfileInfo(string username)
+        {
+            // TODO: get from telerikacademy.com
+            return new RemoteUserProfile
+            {
+                FirstName = "User",
+                LastName = "Userov",
+                Age = 10,
+                City = "Petrich, Kalifornia",
+                LargeAvatarUrl = "some.jpg",
+                Occupation = "Director",
+                Sex = "Male"
             };
         }
     }
