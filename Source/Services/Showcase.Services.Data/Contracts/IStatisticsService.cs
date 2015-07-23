7b90c982dfbea060c12988ee0ed9730d046f152c
Zdravko Georgiev <skinsomnia@gmail.com>
2015-07-23 16:45:47 +0300

    Added GetProjectsByMonth method to StatisticsService

--- Source/Services/Showcase.Services.Data/Contracts/IStatisticsService.cs
+++ Source/Services/Showcase.Services.Data/Contracts/IStatisticsService.cs
@@ -1,9 +1,15 @@
 ﻿namespace Showcase.Services.Data.Contracts
 {
+    using System.Linq;
+
+    using Showcase.Data.Models;
+    using Showcase.Server.DataTransferModels.Statistics;
     using Showcase.Services.Common;
 
     public interface IStatisticsService : IService
     {
         object Current();
+
+        IQueryable<CountByDateModel> ProjectsLastSixMonths();
     }
 }
--- Source/Services/Showcase.Services.Data/StatisticsService.cs
+++ Source/Services/Showcase.Services.Data/StatisticsService.cs
@@ -5,9 +5,14 @@
     using Showcase.Data.Common.Repositories;
     using Showcase.Data.Models;
     using Showcase.Services.Data.Contracts;
+    using System;
+    using Showcase.Server.DataTransferModels.Statistics;
+    using System.Collections.Generic;
 
     public class StatisticsService : IStatisticsService
     {
+        private readonly DateTime ShowcaseLaunchDate = new DateTime(2015, 7, 23);
+
         private IRepository<Project> projects;
 
         public StatisticsService(IRepository<Project> projects)
@@ -17,7 +22,7 @@
 
         public object Current()
         {
-            // TODO: count only approved
+            // TODO: count only approved, add model
             return this.projects
                 .All()
                 .GroupBy(pr => 0)
@@ -29,6 +34,22 @@
                     TotalLikes = gr.Sum(pr => pr.Likes.Count())
                 })
                 .FirstOrDefault();
+        }
+
+        public IQueryable<CountByDateModel> ProjectsLastSixMonths()
+        {
+            var todaySixMonthsAgo = DateTime.Now.AddMonths(-6);
+            
+            return this.projects
+                .All()
+                .Where(p => p.CreatedOn >= todaySixMonthsAgo)
+                .GroupBy(s => s.CreatedOn.Month)
+                .OrderBy(gr => gr.Key)
+                .Select(gr => new CountByDateModel
+                {
+                    Date = gr.FirstOrDefault().CreatedOn,
+                    Count = gr.Count()
+                });
         }
     }
 }
