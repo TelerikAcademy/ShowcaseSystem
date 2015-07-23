bebb88b013e571a226eae660820406f3d280dab7
Zdravko Georgiev <skinsomnia@gmail.com>
2015-07-23 16:47:56 +0300

    Added projects for the past six months chart added to Statistics page

--- Source/Client/Showcase.Client.App/app/statistics-page/statistics-data.js
+++ Source/Client/Showcase.Client.App/app/statistics-page/statistics-data.js
@@ -6,11 +6,14 @@
             return data.get('statistics');
         }
 
-
+        function getProjectsForLastSixMonths() {
+            return data.get('statistics/projectslastsixmonths');
+        }
 
 
         return {
-            getMainStatistics: getMainStatistics
+            getMainStatistics: getMainStatistics,
+            getProjectsForLastSixMonths: getProjectsForLastSixMonths
         };
     };
 
