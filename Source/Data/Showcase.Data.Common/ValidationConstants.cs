809296acc9f1c889e1c6e614378e8158d518e145
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-24 16:55:13 +0300

    Added ValidationConstants for common validation numbers

---
+++ Source/Data/Showcase.Data.Common/ValidationConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Showcase.Data.Common
+{
+    public class ValidationConstants
+    {
+        // Project
+        public const int MinProjectTitleLength = 3;
+        public const int MaxProjectTitleLength = 20;
+        public const int MinProjectDescriptionLength = 100;
+        public const int MaxProjectDescriptionLength = 500;
+        public const int MaxProjectUrlLength = 200;
+    }
+}
