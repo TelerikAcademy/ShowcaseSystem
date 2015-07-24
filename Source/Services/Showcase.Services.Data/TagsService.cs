e6ca29d730b0d911f25e085ba87b671e1d03fb39
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-24 12:55:45 +0300

    Added TagsService.cs

---
+++ Source/Services/Showcase.Services.Data/TagsService.cs
@@ -0,0 +1,25 @@
+﻿namespace Showcase.Services.Data
+{
+    using System.Linq;
+
+    using Showcase.Data.Common.Repositories;
+    using Showcase.Data.Models;
+    using Showcase.Services.Data.Contracts;
+
+    public class TagsService : ITagsService
+    {
+        private readonly IRepository<Tag> tags;
+
+        public TagsService(IRepository<Tag> tags)
+        {
+            this.tags = tags;
+        }
+
+        public IQueryable<Tag> SearchByName(string name)
+        {
+            return this.tags
+                .All()
+                .Where(t => t.Name.ToLower().Contains(name.ToLower()));
+        }
+    }
+}
