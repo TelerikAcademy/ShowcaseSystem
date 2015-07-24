ba3aec4a45a28998b226a4ca9dc8428ed655a2f5
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-24 16:46:16 +0300

    Renamed data model Content to Description

--- Source/Data/Showcase.Data.Models/Project.cs
+++ Source/Data/Showcase.Data.Models/Project.cs
@@ -27,7 +27,7 @@
 
         public string Title { get; set; }
 
-        public string Content { get; set; }
+        public string Description { get; set; }
 
         public string RepositoryUrl { get; set; }
 
--- Source/Server/Showcase.Server.DataTransferModels/Project/ProjectRequestModel.cs
+++ Source/Server/Showcase.Server.DataTransferModels/Project/ProjectRequestModel.cs
@@ -6,7 +6,7 @@
     {
         public string Title { get; set; }
 
-        public string Content { get; set; }
+        public string Description { get; set; }
 
         public string Collaborators { get; set; }
 
