d9c2347bd21d3261bf41ca0fa8733765a1a1ff5e
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-24 12:53:29 +0300

    Added ITagsService.cs

---
+++ Source/Services/Showcase.Services.Data/Contracts/ITagsService.cs
@@ -0,0 +1,12 @@
+﻿namespace Showcase.Services.Data.Contracts
+{
+    using System.Linq;
+
+    using Showcase.Services.Common;
+    using Showcase.Data.Models;
+
+    public interface ITagsService : IService
+    {
+        IQueryable<Tag> SearchByName(string name);
+    }
+}
