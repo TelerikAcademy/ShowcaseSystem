0559ab669f2914646f52046173757c9397fbab20
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-24 15:50:41 +0300

    Added FileRequestModel and ProjectRequestModel

---
+++ Source/Server/Showcase.Server.DataTransferModels/Project/FileRequestModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Showcase.Server.DataTransferModels.Project
+{
+    using System;
+
+    public class FileRequestModel
+    {
+        public string OriginalName { get; set; }
+
+        public string FileExtension { get; set; }
+
+        public string Base64Content { get; set; }
+
+        public byte[] ByteArrayContent
+        {
+            get
+            {
+                return Convert.FromBase64String(this.Base64Content);
+            }
+        }
+    }
+}
---
+++ Source/Server/Showcase.Server.DataTransferModels/Project/ProjectRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Showcase.Server.DataTransferModels.Project
+{
+    using System.Collections.Generic;
+
+    public class ProjectRequestModel
+    {
+        public string Title { get; set; }
+
+        public IEnumerable<FileRequestModel> Images { get; set; }
+    }
+}
