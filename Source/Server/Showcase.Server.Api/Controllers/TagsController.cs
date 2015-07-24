018847bd6abd40bf913ad717627e131cbe125ef5
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-24 13:09:23 +0300

    Added TagsController with Action for searching tags

---
+++ Source/Server/Showcase.Server.Api/Controllers/TagsController.cs
@@ -0,0 +1,42 @@
+﻿namespace Showcase.Server.Api.Controllers
+{
+    using System.Linq;
+    using System.Web.Http;
+
+    using AutoMapper.QueryableExtensions;
+
+    using Showcase.Services.Data.Contracts;
+    using Showcase.Server.Api.Infrastructure.Extensions;
+    using Showcase.Server.DataTransferModels.Tag;
+
+    public class TagsController : BaseController
+    {
+        private const int MinimumCharactersForNameSearch = 2;
+
+        private readonly ITagsService tagsService;
+
+        public TagsController(ITagsService tagsService)
+        {
+            this.tagsService = tagsService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("Search")]
+        public IHttpActionResult Search(string name)
+        {
+            if (string.IsNullOrEmpty(name) || name.Length < MinimumCharactersForNameSearch)
+            {
+                return this.Data(false, string.Format("Name should be at least {0} symbols long", MinimumCharactersForNameSearch));
+            }
+
+            var model = this.tagsService
+                .SearchByName(name)
+                .Project()
+                .To<TagAutocompleteResponseModel>()
+                .ToList();
+
+            return this.Ok(model);
+        }
+    }
+}
