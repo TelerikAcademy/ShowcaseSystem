3f705b4113fd33b14f285040c170be7ad6267148
evlogihr <evlogihristov@yahoo.com>
2015-07-24 08:54:23 +0300

    Added Collaborators and Tags properties to Projects simple model

--- Source/Server/Showcase.Server.DataTransferModels/Project/ProjectResponseSimpleModel.cs
+++ Source/Server/Showcase.Server.DataTransferModels/Project/ProjectResponseSimpleModel.cs
@@ -44,6 +44,10 @@
             }
         }
 
+        public IEnumerable<string> Collaborators { get; set; }
+
+        public IEnumerable<TagResponseModel> Tags { get; set; }
+
         public void CreateMappings(IConfiguration configuration)
         {
             configuration.CreateMap<Project, ProjectResponseSimpleModel>()
@@ -51,7 +55,8 @@
                 .ForMember(pr => pr.Likes, opt => opt.MapFrom(pr => pr.Likes.Count))
                 .ForMember(pr => pr.Visits, opt => opt.MapFrom(pr => pr.Visits.Count))
                 .ForMember(pr => pr.Comments, opt => opt.MapFrom(pr => pr.Comments.Count))
-                .ForMember(pr => pr.MainImageUrl, opt => opt.MapFrom(pr => pr.MainImage.Url + "." + pr.MainImage.FileExtension));
+                .ForMember(pr => pr.MainImageUrl, opt => opt.MapFrom(pr => pr.MainImage.Url + "." + pr.MainImage.FileExtension))
+                .ForMember(pr => pr.Collaborators, opt => opt.MapFrom(pr => pr.Collaborators.Select(c => c.UserName).OrderBy(c => c)));
         }
     }
 }
