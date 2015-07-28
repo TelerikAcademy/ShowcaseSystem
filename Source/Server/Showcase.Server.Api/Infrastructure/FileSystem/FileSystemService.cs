8f7db93cc32e19a858f9d566394f7d7a22f49271
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-28 20:01:51 +0300

    Added FileSystemService.cs

---
+++ Source/Server/Showcase.Server.Api/Infrastructure/FileSystem/FileSystemService.cs
@@ -0,0 +1,18 @@
+﻿namespace Showcase.Server.Api.Infrastructure.FileSystem
+{
+    using System.IO;
+    using System.Web.Hosting;
+
+    public class FileSystemService : IFileSystemService
+    {
+        private const string ImagesServerPath = "~/Images/{0}_{1}.jpg";
+
+        public void SaveImageToFile(byte[] imageContent, string path, string resolution)
+        {
+            var filePath = HostingEnvironment.MapPath(string.Format(ImagesServerPath, path, resolution));
+            var fileInfo = new FileInfo(filePath);
+            fileInfo.Directory.Create();
+            File.WriteAllBytes(filePath, imageContent);
+        }
+    }
+}
---
+++ Source/Server/Showcase.Server.Api/Infrastructure/FileSystem/IFileSystemService.cs
@@ -0,0 +1,7 @@
+﻿namespace Showcase.Server.Api.Infrastructure.FileSystem
+{
+    public interface IFileSystemService
+    {
+        public void SaveImageToFile(byte[] imageContent, string path, string resolution);
+    }
+}
