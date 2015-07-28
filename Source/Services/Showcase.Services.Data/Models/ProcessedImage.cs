8cd84f27d574fa0abb135c8132387a76f9596023
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-28 18:45:17 +0300

    Added ProcessedImage.cs inheriting from Image.cs in order to add byte array contents

---
+++ Source/Services/Showcase.Services.Data/Models/ProcessedImage.cs
@@ -0,0 +1,27 @@
+﻿namespace Showcase.Services.Data.Models
+{
+    using Showcase.Data.Models;
+
+    public class ProcessedImage : Image
+    {
+        public const int ThumbnailImageWidth = 260;
+        public const int HighResolutionWidth = 1360;
+
+        public static ProcessedImage FromImage(Image image, byte[] thumbnailContent, byte[] highResolutionContent)
+        {
+            return new ProcessedImage
+            {
+                Id = image.Id,
+                OriginalFileName = image.OriginalFileName,
+                FileExtension = image.FileExtension,
+                UrlPath = image.UrlPath,
+                ThumbnailContent = thumbnailContent,
+                HighResolutionContent = highResolutionContent
+            };
+        }
+
+        public byte[] ThumbnailContent { get; set; }
+
+        public byte[] HighResolutionContent { get; set; }
+    }
+}
