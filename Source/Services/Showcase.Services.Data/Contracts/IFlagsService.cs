f903a8ad7f69fe177507f1228e0474aeb16ff6b6
Zdravko Georgiev <skinsomnia@gmail.com>
2015-07-29 14:23:47 +0300

    Added FlagsService and interface

---
+++ Source/Services/Showcase.Services.Data/Contracts/IFlagsService.cs
@@ -0,0 +1,9 @@
+﻿namespace Showcase.Services.Data.Contracts
+{
+    public interface IFlagsService
+    {
+        void FlagProject(int projectId, string username);
+
+        void UnFlagProject(int projectId, string username);
+    }
+}
---
+++ Source/Services/Showcase.Services.Data/FlagsService.cs
@@ -0,0 +1,58 @@
+﻿namespace Showcase.Services.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Showcase.Services.Data.Contracts;
+    using Showcase.Data.Common.Repositories;
+    using Showcase.Data.Models;
+
+    public class FlagsService : IFlagsService
+    {
+        private readonly IRepository<Flag> flags;
+        private readonly IUsersService users;
+
+        public FlagsService(IRepository<Flag> flags, IUsersService users)
+        {
+            this.flags = flags;
+            this.users = users;
+        }
+
+        public void FlagProject(int projectId, string username)
+        {
+            var userId = this.users.GetUserId(username);
+
+            if (userId != 0)
+            {
+                var flag = new Flag
+                {
+                    CreatedOn = DateTime.Now,
+                    ProjectId = projectId,
+                    UserId = userId
+                };
+
+                this.flags.Add(flag);
+                this.flags.SaveChanges();
+            }
+        }
+
+        public void UnFlagProject(int projectId, string username)
+        {
+            var userId = this.users.GetUserId(username);
+
+            if (userId != 0)
+            {
+                var flag = this.flags
+                    .All()
+                    .Where(f => f.UserId == userId && f.ProjectId == projectId)
+                    .FirstOrDefault();
+
+                this.flags.Delete(flag);
+                this.flags.SaveChanges();
+            }
+        }
+    }
+}
