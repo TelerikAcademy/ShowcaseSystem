ca277b41f4ef4521fcace26d4610960811490499
Zdravko Georgiev <skinsomnia@gmail.com>
2015-07-30 13:43:43 +0300

    Added validations, when posting or editting comments

--- Source/Server/Showcase.Server.DataTransferModels/Project/CommentRequestModel.cs
+++ Source/Server/Showcase.Server.DataTransferModels/Project/CommentRequestModel.cs
@@ -1,7 +1,14 @@
 ﻿namespace Showcase.Server.DataTransferModels.Project
 {
+    using System.ComponentModel.DataAnnotations;
+
+    using Showcase.Data.Common;
+
     public class CommentRequestModel
     {
+        [Required]
+        [MinLength(ValidationConstants.MinCommentContentLength)]
+        [MaxLength(ValidationConstants.MaxCommentContentLength)]
         public string CommentText { get; set; }
     }
 }
