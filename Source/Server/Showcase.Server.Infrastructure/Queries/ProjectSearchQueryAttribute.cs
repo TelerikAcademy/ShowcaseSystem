09445e6877abe77ee5739e70e64f8a965edcfc6a
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-31 11:57:35 +0300

    EnableQuery moved to custom attribute

---
+++ Source/Server/Showcase.Server.Infrastructure/Queries/ProjectSearchQueryAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace Showcase.Server.Infrastructure.Queries
+{
+    using System.Web.OData;
+    using System.Web.OData.Query;
+
+    using Showcase.Server.Common;
+
+    public class ProjectSearchQueryAttribute : EnableQueryAttribute
+    {
+        public ProjectSearchQueryAttribute()
+        {
+            this.MaxTop = Constants.MaxProjectsPageSize;
+            this.AllowedFunctions = AllowedFunctions.Any | AllowedFunctions.SubstringOf;
+            this.AllowedQueryOptions = AllowedQueryOptions.Top
+                | AllowedQueryOptions.Skip
+                | AllowedQueryOptions.Filter
+                | AllowedQueryOptions.OrderBy
+                | AllowedQueryOptions.Count;
+        }
+    }
+}
