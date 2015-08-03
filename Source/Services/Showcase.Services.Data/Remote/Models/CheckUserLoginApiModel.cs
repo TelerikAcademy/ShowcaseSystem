71a4f89e94e6ad4041a7bd3763fd686ae5c66a59
Nikolay Kostov <admin@nikolay.it>
2015-08-03 17:06:45 +0300

    Implemented remote login to telerikacademy.com

---
+++ Source/Services/Showcase.Services.Data/Remote/Models/CheckUserLoginApiModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Showcase.Services.Data.Remote.Models
+{
+    public class CheckUserLoginApiModel
+    {
+        public bool IsValid { get; set; }
+
+        public string UserName { get; set; }
+
+        public string SmallAvatarUrl { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
--- Source/Services/Showcase.Services.Data/RemoteDataService.cs
+++ Source/Services/Showcase.Services.Data/RemoteDataService.cs
@@ -1,24 +1,50 @@
 ﻿namespace Showcase.Services.Data
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Net.Http;
     using System.Threading.Tasks;
+
+    using Newtonsoft.Json;
 
     using Showcase.Data.Models;
     using Showcase.Services.Data.Contracts;
     using Showcase.Services.Data.Models;
+    using Showcase.Services.Data.Remote.Models;
 
     public class RemoteDataService : IRemoteDataService
     {
-        public Task<User> Login(string username, string password)
+#if DEBUG
+        private const string BaseAddress = "http://localhost:1337";
+#else
+        private const string BaseAddress = "https://telerikacademy.com";
+#endif
+        private const string ApiCheckUserLoginUrlFormat = "/Api/Users/CheckUserLogin?usernameoremail={0}&password={1}";
+
+        private readonly HttpClient client;
+
+        public RemoteDataService()
         {
-            // TODO: implement and get from telerikacademy.com
-            return Task.Run(() => new User
-            {
-                UserName = username,
-                AvatarUrl = "some url", // return small avatar URL here 
-                IsAdmin = true
-            });
+            this.client = new HttpClient { BaseAddress = new Uri(BaseAddress) };
+            this.client.DefaultRequestHeaders.Add("Connection", "close");
+        }
+
+        public async Task<User> Login(string username, string password)
+        {
+            var url = string.Format(ApiCheckUserLoginUrlFormat, username, password);
+            var response = await this.client.GetAsync(url);
+            var jsonString = await response.Content.ReadAsStringAsync();
+            var model = JsonConvert.DeserializeObject<CheckUserLoginApiModel>(jsonString);
+
+            return !model.IsValid
+                       ? null
+                       : new User
+                             {
+                                 UserName = model.UserName,
+                                 AvatarUrl = model.SmallAvatarUrl,
+                                 IsAdmin = model.IsAdmin
+                             };
         }
 
         public Task<IEnumerable<User>> UsersInfo(IEnumerable<string> usernames)
