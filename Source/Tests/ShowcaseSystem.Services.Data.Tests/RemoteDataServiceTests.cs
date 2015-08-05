2f4ac6490c418801b0e982a1d30224595fffe4e9
Nikolay Kostov <admin@nikolay.it>
2015-08-05 17:36:56 +0300

    Added more cases for RemoteDataService.Login method

--- Source/Tests/ShowcaseSystem.Services.Data.Tests/RemoteDataServiceTests.cs
+++ Source/Tests/ShowcaseSystem.Services.Data.Tests/RemoteDataServiceTests.cs
@@ -2,11 +2,21 @@
 {
     using NUnit.Framework;
 
+    using Showcase.Data.Models;
     using Showcase.Services.Data;
 
     [TestFixture]
     public class RemoteDataServiceTests
     {
+        [TestCase(null, "ShowcaseSystem", "invalidpassword")]
+        [TestCase(null, "invalid*user", "invalidpassword")]
+        public void LoginShouldReturnValidResults(User expected, string username, string password)
+        {
+            var service = new RemoteDataService();
+            var result = service.Login(username, password).Result;
+            Assert.AreEqual(expected, result);
+        }
+
         [TestCase(true, "ShowcaseSystem")]
         [TestCase(true, "Nikolay.IT", "ivaylo.kenov", "ShowcaseSystem")]
         [TestCase(false, "ShowcaseSystem", "but*this*user*does*not*exist")]
