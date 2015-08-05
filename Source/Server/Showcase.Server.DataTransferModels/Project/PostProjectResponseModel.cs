b71e6658e0b77e28d523ce2adf4e13aab720e0e1
Zdravko Georgiev <skinsomnia@gmail.com>
2015-08-05 18:16:39 +0300

    Fixed bug, where after posting a project, the redirect Url used the project title instead of the project TitleUrl

--- Source/Server/Showcase.Server.DataTransferModels/Project/PostProjectResponseModel.cs
+++ Source/Server/Showcase.Server.DataTransferModels/Project/PostProjectResponseModel.cs
@@ -1,12 +1,21 @@
 ﻿namespace Showcase.Server.DataTransferModels.Project
 {
+    using AutoMapper;
+    using MissingFeatures;
+
     using Showcase.Data.Models;
     using Showcase.Server.Common.Mapping;
 
-    public class PostProjectResponseModel : IMapFrom<Project>
+    public class PostProjectResponseModel : IMapFrom<Project>, IHaveCustomMappings
     {
         public int Id { get; set; }
 
-        public string Title { get; set; }
+        public string TitleUrl { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<Project, PostProjectResponseModel>()
+                .ForMember(p => p.TitleUrl, opt => opt.MapFrom(p => p.Title.ToUrl()));
+        }
     }
 }
