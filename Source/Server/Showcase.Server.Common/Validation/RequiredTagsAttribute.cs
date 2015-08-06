5de2ee6a4dd9e811a4014cdd51d1fe77943ccf98
Zdravko Georgiev <skinsomnia@gmail.com>
2015-08-06 15:18:42 +0300

    Added RequiredRagsAttribute

---
+++ Source/Server/Showcase.Server.Common/Validation/RequiredTagsAttribute.cs
@@ -0,0 +1,39 @@
+﻿namespace Showcase.Server.Common.Validation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Ninject;
+
+    using Showcase.Data.Common;
+    using Showcase.Services.Data.Contracts;
+
+    public class RequiredTagsAttribute : ValidationAttribute
+    {
+        public RequiredTagsAttribute()
+        {
+            this.ErrorMessage = ValidationConstants.RequiredTagsErrorMessage;
+        }
+
+        [Inject]
+        public ITagsService TagsService { private get; set; }
+
+        public override bool IsValid(object value)
+        {
+            var valueAsString = value as string;
+            if (valueAsString != null)
+            {
+                int currentTagId;
+                var tagsIds = valueAsString.Split(',').Where(t => int.TryParse(t, out currentTagId)).Select(t => int.Parse(t)).ToList();
+                var task = Task.Run(async () => await this.TagsService.AllRequiredTagsArePresent(tagsIds));
+                return task.Result;
+            }
+
+            return true;
+        }
+    }
+}
