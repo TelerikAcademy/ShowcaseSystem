fc3c66574f4c0d0c1dae05e4ec7f9cf0ea1e5ab7
Zdravko Georgiev <skinsomnia@gmail.com>
2015-08-06 19:40:26 +0300

    Added flag/unflag comment inside user profile comments

--- Source/Client/Showcase.Client.App/app/user-profile-page/user-profile-comments-directive.js
+++ Source/Client/Showcase.Client.App/app/user-profile-page/user-profile-comments-directive.js
@@ -44,6 +44,30 @@
                 scope.cancelEdit = function (id) {
                     scope.edittingComments[id] = false;
                 };
+                
+                scope.flagComment = function (commentId) {
+                    commentsData.flagComment(commentId)
+                        .then(function (data) {
+                            for (var i = 0; i < scope.comments.length; i++) {
+                                if (scope.comments[i].id == commentId) {
+                                    scope.comments[i].isFlagged = true;
+                                    break;
+                                }
+                            }
+                        });
+                };
+
+                scope.unFlagComment = function (commentId) {
+                    commentsData.unFlagComment(commentId)
+                        .then(function (data) {
+                            for (var i = 0; i < scope.comments.length; i++) {
+                                if (scope.comments[i].id == commentId) {
+                                    scope.comments[i].isFlagged = false;
+                                    break;
+                                }
+                            }
+                        });
+                };
 
                 scope.saveComment = function (id, text) {
                     if (text.length < 10 || text.length > 500) {
