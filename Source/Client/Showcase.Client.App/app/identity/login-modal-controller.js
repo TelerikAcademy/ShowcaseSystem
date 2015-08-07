d79e4e64fe6e7ca8337689c30c156884defbc58b
Zdravko Georgiev <skinsomnia@gmail.com>
2015-08-07 18:34:22 +0300

    UI improvements

--- Source/Client/Showcase.Client.App/app/identity/login-modal-controller.js
+++ Source/Client/Showcase.Client.App/app/identity/login-modal-controller.js
@@ -4,7 +4,8 @@
     var loginModalController = function loginModalController($scope, auth) {
         var vm = this;
 
-        $('#login-form').find('input').keypress(function (e) {
+        // TODO: directive, someone?
+        angular.element('#login-form').find('input').keypress(function (e) {
             if (e.which == 10 || e.which == 13) {
                 $('#login-btn').click();
             }
