16e6cf62088429459615193420bf965e18c4bd82
ivaylokenov <ivaylo.kenov@gmail.com>
2015-08-11 10:50:50 +0300

    gulpfile.js is now ready for production and development environments

--- README.md
+++ README.md
@@ -21,4 +21,8 @@ Showcase system for students projects in Telerik Academy
 
 		gulp watch
 		
+- For minified scripts and styles use
+
+		gulp watch --env production
+		
 - Or optionally use the [Task Runner Explorer](https://visualstudiogallery.msdn.microsoft.com/8e1b4368-4afb-467a-bc13-9650572db708) extension to Visual Studio 2013 which is already configured to run the Gulp watch task on every solution build
--- Source/Client/Showcase.Client.App/gulpfile.js
+++ Source/Client/Showcase.Client.App/gulpfile.js
@@ -1,6 +1,8 @@
 /// <vs />
 // Include plug-ins
-var gulp = require('gulp'),
+var isProduction = require('yargs').argv.env === 'production',
+	gulp = require('gulp'),
+	gulpIf = require('gulp-if'),
 	jshint = require('gulp-jshint'),
 	concat = require('gulp-concat'),
 	minifyCSS = require('gulp-minify-css'),
@@ -11,8 +13,8 @@ var gulp = require('gulp'),
 	minifyHTML = require('gulp-minify-html'),
 	del = require('del'),
 	angularFilesort = require('gulp-angular-filesort');
- 
- // File paths
+
+// File paths
 var config = {
     vendorJsSrc: [
         'scripts/epona/plugins/jquery-2.1.3.min.js',
@@ -33,7 +35,7 @@ var config = {
         'Scripts/angular-ui/ui-bootstrap-tpls.js'
     ],
     appJsSrc: ['app/**/*.js'],
-	appCssSrc: [
+    appCssSrc: [
         'content/epona/css/font-awesome.css',
         'content/epona/css/sky-forms.css',
         'scripts/epona/plugins/owl-carousel/owl.pack.css',
@@ -49,37 +51,36 @@ var config = {
         'content/token-input-showcase.css',
         'content/toastr.css',
         'content/main.css'],
-	appIndexHtml: 'index-template.html'
+    appIndexHtml: 'index-template.html'
 }
 
 // For browser caching
-var getStamp = function() {
-  var myDate = new Date();
+var getStamp = function () {
+    var myDate = new Date();
 
-  var myYear = myDate.getFullYear().toString();
-  var myMonth = ('0' + (myDate.getMonth() + 1)).slice(-2);
-  var myDay = ('0' + myDate.getDate()).slice(-2);
-  var mySeconds = myDate.getSeconds().toString();
+    var myYear = myDate.getFullYear().toString();
+    var myMonth = ('0' + (myDate.getMonth() + 1)).slice(-2);
+    var myDay = ('0' + myDate.getDate()).slice(-2);
+    var mySeconds = myDate.getSeconds().toString();
 
-  var myFullDate = myYear + myMonth + myDay + mySeconds;
+    var myFullDate = myYear + myMonth + myDay + mySeconds;
 
-  return myFullDate;
+    return myFullDate;
 };
 
- // Minify, prefix and contat CSS
-gulp.task('css', function(){
-	del.sync(['app/build/allcss*']);
+// Minify, prefix and contat CSS
+gulp.task('css', function () {
+    del.sync(['app/build/allcss*']);
 
     return gulp.src(config.appCssSrc)
-		.pipe(minifyCSS())
+		.pipe(gulpIf(isProduction, minifyCSS()))
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
-		.pipe(concat('allcss' + getStamp() + '.min.css', {newLine: ''}))
-		// .pipe(concat('allcss.min.css', {newLine: ''}))
+		.pipe(concat('allcss' + (isProduction ? getStamp() : '') + '.min.css', { newLine: '' }))
 		.pipe(gulp.dest('app/build'))
 });
- 
+
 // Lint Task
-gulp.task('lint', function() {
+gulp.task('lint', function () {
     return gulp.src(config.appJsSrc)
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
@@ -90,21 +91,19 @@ gulp.task('vendors', function () {
     del.sync(['app/build/vendorjs*']);
 
     return gulp.src(config.vendorJsSrc)
-		.pipe(uglify())
-		.pipe(concat('vendorjs' + getStamp() + '.min.js', { newLine: '' }))
-		// .pipe(concat('alljs.min.js', {newLine: ''}))
+		.pipe(gulpIf(isProduction, uglify()))
+		.pipe(concat('vendorjs' + (isProduction ? getStamp() : '') + '.min.js', { newLine: '' }))
 		.pipe(gulp.dest('app/build'));
 });
 
 // Combine and minify all JS files from the app folder
-gulp.task('scripts', function() {
-	del.sync(['app/build/alljs*']);
-	
-	return gulp.src(config.appJsSrc)
+gulp.task('scripts', function () {
+    del.sync(['app/build/alljs*']);
+
+    return gulp.src(config.appJsSrc)
 		.pipe(angularFilesort())
-		.pipe(uglify())
-		.pipe(concat('alljs' + getStamp() + '.min.js', {newLine: ''}))
-		// .pipe(concat('alljs.min.js', {newLine: ''}))
+		.pipe(gulpIf(isProduction, uglify()))
+		.pipe(concat('alljs' + (isProduction ? getStamp() : '') + '.min.js', { newLine: '' }))
 		.pipe(gulp.dest('app/build'));
 });
 
@@ -113,9 +112,9 @@ gulp.task('html', ['css', 'scripts'], function () {
     del.sync(['index.html']);
     var target = gulp.src(config.appIndexHtml);
     var vendorSources = gulp.src(['app/build/vendorjs*'], { read: false });
-	var appSources = gulp.src(['app/build/alljs*', 'app/build/allcss*'], { read: false });
-	
-	return target
+    var appSources = gulp.src(['app/build/alljs*', 'app/build/allcss*'], { read: false });
+
+    return target
         .pipe(inject(vendorSources, { starttag: '<!-- inject:vendors:{{ext}} -->' }))
         .pipe(inject(appSources))
 		.pipe(minifyHTML({ conditionals: true }))
@@ -125,10 +124,10 @@ gulp.task('html', ['css', 'scripts'], function () {
 
 // Watch for changes
 gulp.task('watch', ['lint', 'css', 'vendors', 'scripts', 'html'], function () {
-	gulp.watch(config.appCssSrc, ['css', 'html']);
-	gulp.watch(config.appJsSrc, ['lint', 'scripts', 'html']);
-	gulp.watch(config.appIndexHtml, ['html']);
+    gulp.watch(config.appCssSrc, ['css', 'html']);
+    gulp.watch(config.appJsSrc, ['lint', 'scripts', 'html']);
+    gulp.watch(config.appIndexHtml, ['html']);
 });
- 
+
 // Set  default tasks
-gulp.task('default', ['lint', 'css', 'vendors', 'scripts', 'html'], function(){});
+gulp.task('default', ['lint', 'css', 'vendors', 'scripts', 'html'], function () { });
