6ab055b4724790b225b3cbff6d9ec6c59ef843bb
ivaylokenov <ivaylo.kenov@gmail.com>
2015-08-11 14:51:11 +0300

    Added FileInfoResponseModel.cs

---
+++ Source/Server/Showcase.Server.DataTransferModels/File/FileInfoResponseModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Showcase.Server.DataTransferModels.File
+{
+    using Showcase.Data.Models;
+    using Showcase.Server.Common.Mapping;
+
+    public class FileInfoResponseModel : IMapFrom<FileInfo>
+    {
+        public string OriginalFileName { get; set; }
+
+        public string FileExtension { get; set; }
+
+        public string UrlPath { get; set; }
+    }
+}
