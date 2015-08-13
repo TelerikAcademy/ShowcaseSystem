28439b32b0234f42e2abd0462d86818b2f1fb227
ivaylokenov <ivaylo.kenov@gmail.com>
2015-08-13 13:39:42 +0300

    Added CharExtensions.cs for IsEnglishLetter extension method

---
+++ Source/Services/Showcase.Services.Common/Extensions/CharExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Showcase.Services.Common.Extensions
+{
+    public static class CharExtensions
+    {
+        public static bool IsEnglishLetter(this char symbol)
+        {
+            return !((symbol < 65 || 90 < symbol) && (symbol < 97 || 122 < symbol));
+        }
+    }
+}
