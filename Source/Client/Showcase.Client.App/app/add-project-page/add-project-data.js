fac95d2d68896edc895532f24ab0041f9ffd1fa8
ivaylokenov <ivaylo.kenov@gmail.com>
2015-08-13 11:37:02 +0300

    Added data for getting different kinds of tags

--- Source/Client/Showcase.Client.App/app/add-project-page/add-project-data.js
+++ Source/Client/Showcase.Client.App/app/add-project-page/add-project-data.js
@@ -2,11 +2,21 @@
     'use strict';
 
     var addProjectData = function addProjectData(data) {
+        function getSeasonTags() {
+            return data.get('tags/allSeasonTags');
+        }
+
+        function getLanguageAndTechnologyTags() {
+            return data.get('tags/allLanguageAndTechnologyTags');
+        }
+
         function addProject(project) {
             return data.post('projects', project);
         }
 
         return {
+            getSeasonTags: getSeasonTags,
+            getLanguageAndTechnologyTags: getLanguageAndTechnologyTags,
             addProject: addProject
         };
     };
