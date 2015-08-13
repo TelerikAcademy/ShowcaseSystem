4f736f2900cc744be6d3cf92789c31c381832c44
ivaylokenov <ivaylo.kenov@gmail.com>
2015-08-13 13:40:34 +0300

    Added two validation attributes for uploaded files

---
+++ Source/Server/Showcase.Server.Common/Validation/UploadedDownloadableFilesCollectionAttribute.cs
@@ -0,0 +1,40 @@
+﻿namespace Showcase.Server.Common.Validation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using Showcase.Server.Common.Validation.Base;
+    using Showcase.Services.Common.Extensions;
+
+    public class UploadedDownloadableFilesCollectionAttribute : UploadedFilesCollectionAttribute
+    {
+        private const char WhiteSpace = ' ';
+
+        private readonly List<string> AllowedImageExtensions = new List<string> { "pptx", "ppt", "pdf", "txt", "doc", "docx", "zip", "rar", "7z" };
+
+        public UploadedDownloadableFilesCollectionAttribute()
+        {
+            this.AllowedExtensions = this.AllowedImageExtensions;
+        }
+
+        public override bool IsValid(object value)
+        {
+            return base.IsValid(value,
+                file => this.AllowedExtensions.Contains(file.FileExtension.ToLower()),
+                file => this.ValidateBase64StringSize(file.Base64Content),
+                file => this.ContainsOnlyEnglishLettersAndWhiteSpace(file.OriginalFileName));
+        }
+
+        private bool ContainsOnlyEnglishLettersAndWhiteSpace(string fileName)
+        {
+            var symbolsWithoutWhiteSpace = string.Join(string.Empty, fileName.Split(new[] { WhiteSpace }, StringSplitOptions.RemoveEmptyEntries));
+            foreach (var symbol in symbolsWithoutWhiteSpace)
+            {
+                if (!symbol.IsEnglishLetter()) return false;
+            }
+
+            return true;
+        }
+    }
+}
--- Source/Server/Showcase.Server.Common/Validation/UploadedImagesCollectionAttribute.cs
+++ Source/Server/Showcase.Server.Common/Validation/UploadedImagesCollectionAttribute.cs
@@ -4,46 +4,25 @@
     using System.ComponentModel.DataAnnotations;
 
     using Showcase.Server.Common.Models;
+    using Showcase.Server.Common.Validation.Base;
 
-    public class UploadedImagesCollectionAttribute : ValidationAttribute
+    public class UploadedImagesCollectionAttribute : UploadedFilesCollectionAttribute
     {
         private const int MinImageSize = 51200;
-        private const int MaxImageSize = 10485760;
 
         private readonly List<string> AllowedImageExtensions = new List<string> { "jpg", "jpeg", "gif", "png", "bmp" };
 
+        public UploadedImagesCollectionAttribute()
+        {
+            this.AllowedExtensions = this.AllowedImageExtensions;
+            this.MinFileSize = MinImageSize;
+        }
+
         public override bool IsValid(object value)
         {
-            var collectionOfImages = value as ICollection<FileRequestModel>;
-            if (collectionOfImages != null)
-            {
-                foreach (var image in collectionOfImages)
-                {
-                    if (!this.AllowedImageExtensions.Contains(image.FileExtension.ToLower())) return false;
-                    if (!this.ValidateBase64StringSize(image.Base64Content)) return false;
-                }
-
-                return true;
-            }
-
-            return true;
-        }
-
-        public override string FormatErrorMessage(string name)
-        {
-            return string.Format(this.ErrorMessage, MinImageSize / 1024.0, MaxImageSize / 1024.0 / 1024);
-        }
-
-        private bool ValidateBase64StringSize(string content)
-        {
-            var trimmedContent = content.Trim('=');
-            var fileSize = trimmedContent.Length * 3 / 4.0;
-            if (fileSize < MinImageSize || fileSize > MaxImageSize)
-            {
-                return false;
-            }
-
-            return true;
+            return base.IsValid(value,
+                image => this.AllowedExtensions.Contains(image.FileExtension.ToLower()),
+                image => this.ValidateBase64StringSize(image.Base64Content));
         }
     }
 }
