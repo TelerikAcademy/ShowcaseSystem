76203dd20f4dca08279b7e3f6000543ab2a501b0
ivaylokenov <ivaylo.kenov@gmail.com>
2015-08-14 10:59:49 +0300

    Comments are now validated with only English letters (closes #113)

--- Source/Server/Showcase.Server.DataTransferModels/Comment/CommentRequestModel.cs
+++ Source/Server/Showcase.Server.DataTransferModels/Comment/CommentRequestModel.cs
@@ -3,14 +3,17 @@
     using System.ComponentModel.DataAnnotations;
 
     using Showcase.Data.Common;
+    using Showcase.Server.Common.Validation;
 
     public class CommentRequestModel
     {
         public int Id { get; set; }
 
+        [Display(Name = "Comment")]
         [Required]
         [MinLength(ValidationConstants.MinCommentContentLength)]
         [MaxLength(ValidationConstants.MaxCommentContentLength)]
+        [OnlyEnglish]
         public string CommentText { get; set; }
     }
 }
