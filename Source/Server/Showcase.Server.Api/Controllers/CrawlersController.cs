4a2ddf0c091d8f3050975d456d43b5085da414c7
ivaylokenov <ivaylo.kenov@gmail.com>
2015-08-14 14:57:59 +0300

    Added CrawlersController.cs for social network crawlers

---
+++ Source/Server/Showcase.Server.Api/Controllers/CrawlersController.cs
@@ -0,0 +1,34 @@
+﻿namespace Showcase.Server.Api.Controllers
+{
+    using System.Data.Entity;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+
+    using AutoMapper.QueryableExtensions;
+
+    using Showcase.Server.Api.Controllers.Base;
+    using Showcase.Server.DataTransferModels.Project;
+    using Showcase.Services.Data.Contracts;
+
+    public class CrawlersController : BaseController
+    {
+        private readonly IProjectsService projectsService;
+
+        public CrawlersController(IProjectsService projectsService)
+        {
+            this.projectsService = projectsService;
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> Get(int id)
+        {
+            var model = await this.projectsService
+                .ProjectById(id)
+                .Project()
+                .To<ProjectCrawlerResponseModel>()
+                .FirstOrDefaultAsync();
+
+            return Ok(model);
+        }
+    }
+}
