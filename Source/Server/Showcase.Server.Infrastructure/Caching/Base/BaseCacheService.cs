d1042b56d967434b6b37f44c07cf64409924217b
ivaylokenov <ivaylo.kenov@gmail.com>
2015-08-17 19:01:08 +0300

    BaseCacheService.cs made async

--- Source/Server/Showcase.Server.Infrastructure/Caching/Base/BaseCacheService.cs
+++ Source/Server/Showcase.Server.Infrastructure/Caching/Base/BaseCacheService.cs
@@ -1,17 +1,18 @@
 ﻿namespace Showcase.Server.Infrastructure.Caching.Base
 {
     using System;
+    using System.Threading.Tasks;
     using System.Web;
 
     public abstract class BaseCacheService
     {
-        protected T Get<T>(string cacheId, Func<T> getItemCallback) where T : class
+        protected async Task<T> Get<T>(string cacheId, Func<Task<T>> getItemCallback) where T : class
         {
             var item = HttpRuntime.Cache.Get(cacheId) as T;
             if (item == null)
             {
-                item = getItemCallback();
-                HttpContext.Current.Cache.Insert(cacheId, item);
+                item = await getItemCallback();
+                HttpContext.Current.Cache.Insert(cacheId, item); // TODO: lock, add optional timespan
                 return item;
             }
 
