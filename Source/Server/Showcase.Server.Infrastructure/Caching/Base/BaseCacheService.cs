66e973c968a5e812866dd82670ad6f6c1f010ffe
ivaylokenov <ivaylo.kenov@gmail.com>
2015-08-20 10:42:51 +0300

    Added BaseCacheService.cs (#45)

--- Source/Server/Showcase.Server.Infrastructure/Caching/Base/BaseCacheService.cs
+++ Source/Server/Showcase.Server.Infrastructure/Caching/Base/BaseCacheService.cs
@@ -3,16 +3,27 @@
     using System;
     using System.Threading.Tasks;
     using System.Web;
+    using System.Web.Caching;
 
     public abstract class BaseCacheService
     {
-        protected async Task<T> Get<T>(string cacheId, Func<Task<T>> getItemCallback) where T : class
+        protected async Task<T> Get<T>(
+            string cacheId,
+            Func<Task<T>> getItemCallback,
+            DateTime? absoluteExpiration = null,
+            TimeSpan? slidingExpiration = null) 
+            where T : class
         {
             var item = HttpRuntime.Cache.Get(cacheId) as T;
             if (item == null)
             {
                 item = await getItemCallback();
-                HttpContext.Current.Cache.Insert(cacheId, item); // TODO: lock, add optional timespan
+                HttpContext.Current.Cache.Insert(
+                    cacheId,
+                    item,
+                    null,
+                    absoluteExpiration ?? Cache.NoAbsoluteExpiration,
+                    slidingExpiration ?? Cache.NoSlidingExpiration); // TODO: lock
                 return item;
             }
 
