400f740252a4e0ea15df6c0556a140260cc09838
Nikolay Kostov <admin@nikolay.it>
2015-08-24 12:56:37 +0300

    Removed try-catch and useless comments from "RemoteFileExists" test method

--- Source/Tests/ShowcaseSystem.Services.Data.Tests/RemoteDataServiceTests.cs
+++ Source/Tests/ShowcaseSystem.Services.Data.Tests/RemoteDataServiceTests.cs
@@ -7,6 +7,7 @@
 
     using Showcase.Data.Models;
     using Showcase.Services.Data;
+    using System.Diagnostics;
 
     [TestFixture]
     public class RemoteDataServiceTests
@@ -104,26 +105,12 @@
 
         private static bool RemoteFileExists(string url)
         {
-            try
-            {
-                // Creating the HttpWebRequest
-                var request = WebRequest.Create(url) as HttpWebRequest;
-                request.Timeout = 20000;
+            var request = WebRequest.Create(url) as HttpWebRequest;
+            request.Timeout = 20000;
+            request.Method = "HEAD";
                 
-                // Setting the Request method HEAD, you can also use GET too.
-                request.Method = "HEAD";
-                
-                // Getting the Web Response.
-                var response = request.GetResponse() as HttpWebResponse;
-                
-                // Returns TRUE if the Status code == 200
-                return response.StatusCode == HttpStatusCode.OK;
-            }
-            catch
-            {
-                // Any exception will returns false.
-                return false;
-            }
+            var response = request.GetResponse() as HttpWebResponse;
+            return response.StatusCode == HttpStatusCode.OK;
         }
     }
 }
