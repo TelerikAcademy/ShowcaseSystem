1bdf913516868dd5bd048f32da2441a525b04dc0
ivaylokenov <ivaylo.kenov@gmail.com>
2015-09-08 13:27:38 +0300

    Added BaseProjectRequestModel.cs and EditProjectRequestModel.cs (#7)

---
+++ Source/Server/Showcase.Server.DataTransferModels/Project/EditProjectRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Showcase.Server.DataTransferModels.Project
+{
+    using Showcase.Server.Common.Mapping;
+    using Showcase.Data.Models;
+
+    public class EditProjectRequestModel : BaseProjectRequestModel, IMapFrom<Project>
+    {
+        public int Id { get; set; }
+    }
+}
